d4e07f045c6aff41f5bfd2e1e9f3f442462e112e
cesardushime <125463453+cesardushime@users.noreply.github.com>
2024-11-09 16:24:06 +0100

    created prompt to generate sample cover letter

--- mychatbot.py
+++ mychatbot.py
@@ -1,1 +1,15 @@
-print("My first Chatbot...")
+import openai
+
+openai.api_key = "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" 
+
+def chat_withMe(prompt):
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [{"role": "user", "content": prompt}],
+    )
+    return response['choices'][0]['message']['content'].strip()
+
+user_input = "Create a sample cover letter"
+
+response = chat_withMe(user_input)
+print("chatbot: ", response)
